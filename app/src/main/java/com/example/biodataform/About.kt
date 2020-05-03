91af99d161403dbb8c82a2f1d7ef370a3eee731e
Dicky Alamsyah <dickyalamsyah0901@gmail.com>
2020-05-04 02:50:34 +0700

    Create Intent BiodataForm to BiodataProfile Page by results

--- app/src/main/java/com/example/biodataform/About.kt
+++ app/src/main/java/com/example/biodataform/About.kt
@@ -1,4 +1,12 @@
 package com.example.biodataform
 
-class About {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class About : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.about)
+    }
 }
--- app/src/main/java/com/example/biodataform/Profile.kt
+++ app/src/main/java/com/example/biodataform/Profile.kt
@@ -1,4 +1,12 @@
 package com.example.biodataform
 
-class Profile {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Profile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile)
+    }
 }
